b7edad5d1268f5f817200acdcddd798726c79856
unknown <tioyudhoprasetio33@gmail.com>
2025-02-24 11:08:14 +0700

    refactor(analytics): move Google Analytics script to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,23 @@ import "./index.css";
 import App from "./App.tsx";
 import "flowbite";
 
+// Tambahkan Google Analytics
+const GTAG_ID = "G-5B1S66Q62L";
+
+// Buat script untuk memuat Google Analytics
+const script = document.createElement("script");
+script.async = true;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`;
+document.head.appendChild(script);
+
+// Inisialisasi Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(...args: any[]) {
+  window.dataLayer.push(args);
+}
+gtag("js", new Date());
+gtag("config", GTAG_ID);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
